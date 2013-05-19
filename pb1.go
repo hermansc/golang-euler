1b8def5782db470904087f1ceb8e0f1488b5439a
Herman Schistad <herman.schistad@gmail.com>
2013-05-19 05:37:50 +0200

    Remove math library and use simpler expression

--- pb1.go
+++ pb1.go
@@ -2,14 +2,13 @@ package main
 
 import (
   "fmt"
-  "math"
 )
 
 func findSum(lim int) int {
   sum := 0
-  for i := 0; i<lim; i++ {
-    if math.Mod(float64(i),3) == 0 || math.Mod(float64(i), 5) == 0 {
-      sum = sum + i
+  for i:=0;i<lim;i++ {
+    if i%3==0 || i%5==0 {
+      sum += i
     }
   }
   return sum
